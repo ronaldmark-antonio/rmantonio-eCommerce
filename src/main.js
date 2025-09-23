3b4f92338c99e2e8b13c868462761483701e6952
jerrysenas1 <jerrysenas1@gmail.com>
2025-09-23 18:40:35 +0800

    Add bootstrap and notyf packages

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
+import 'notyf/notyf.min.css'
 import './assets/main.css'
 
 import { createApp } from 'vue'
