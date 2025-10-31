2f8c2c93a6a4c2de21da665d9e5f0b89097ee422
ronaldmark.antonio <ronaldmark.antonio@gmail.com>
2025-10-31 18:05:37 +0800

    Debug global.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const api = axios.create();
+const api = axios.create({
+    baseURL: import.meta.env.VITE_JOB_TRACKER_API,
+});
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
